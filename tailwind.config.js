64b8f0285ebf63ae565d74109ef46f75dd55af01
unknown <fboy4069@gmail.com>
2022-06-30 17:16:30 +0600

    project initialised

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        tasktheme: {
+          primary: "#9c1de4",
+          secondary: "#ba4dff",
+          accent: "#eed3ff",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
